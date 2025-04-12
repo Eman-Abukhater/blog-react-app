cbb19ee057094e3540c1db1e1982327889931442
Eman <emanabukhater2001@gmail.com>
2025-04-12 22:15:52 +0300

    add card for each blog

--- blog-app/src/components/BlogCard.jsx
+++ blog-app/src/components/BlogCard.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
-function BlogCard({blog}) {
+function BlogCard({ blog }) {
+  const navigate = useNavigate();
   return (
     <div className="card">
-      <h2>{blog.title}</h2>
+      <h3>{blog.title}</h3>
       <p>Author: {blog.author}</p>
       <p>Tags: {blog.tags}</p>
-      <p>Rating: {blog.rating || 'No ratings yet'}</p>
-
+      <button onClick={() => navigate(`/view/${blog.id}`)}>Read</button>
+      <button onClick={() => navigate(`/edit/${blog.id}`)}>Edit</button>
     </div>
-  )
+  );
 }
 
-export default BlogCard
+export default BlogCard;
