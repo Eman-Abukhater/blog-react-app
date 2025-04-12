a45f9a3ddb85578c8c7aa661d284c55a7491225b
Eman <emanabukhater2001@gmail.com>
2025-04-12 21:49:50 +0300

    add card for each blog

--- blog-app/src/components/BlogCard.jsx
+++ blog-app/src/components/BlogCard.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 function BlogCard({blog}) {
   return (
-    <div>BlogCard</div>
+    <div className="card">
+      <h2>{blog.title}</h2>
+      <p>Author: {blog.author}</p>
+      <p>Tags: {blog.tags}</p>
+      <p>Rating: {blog.rating || 'No ratings yet'}</p>
+
+    </div>
   )
 }
 
