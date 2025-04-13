90be5717e96d489c83d20698aae0c199d2cdd342
Eman <emanabukhater2001@gmail.com>
2025-04-13 19:21:05 +0300

    add Routes in App.jsx

--- blog-app/src/App.jsx
+++ blog-app/src/App.jsx
@@ -1,21 +1,26 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { BlogContext } from './context/BlogContext'; // Import the context
-import BlogProvider from './context/BlogContext';
-import Home from './pages/Home';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BlogContext } from "./context/BlogContext"; // Import the context
+import BlogProvider from "./context/BlogContext";
+import Home from "./pages/Home";
 
 function App() {
-
   return (
     <div>
       <BlogProvider>
         <Router>
+          <nav>
+            <Link to="/">Home</Link> | <Link to="/add">Add Blog</Link>
+          </nav>
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/add" element={<AddBlog />} />
+            <Route path="/edit/:id" element={<EditBlog />} />
+            <Route path="/view/:id" element={<ViewBlog />} />{" "}
           </Routes>
         </Router>
       </BlogProvider>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
